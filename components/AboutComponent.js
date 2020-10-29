019595ff1bf86733d321f6b9d5f808f98d0c4a75
Nikhil Singh <singhyesh2000@gmail.com>
2020-10-29 21:33:06 +0530

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { FlatList, ScrollView, Text, View } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -13,44 +14,14 @@ const mapStateToProps = state => {
 function History(props) {
     return (
         <Card title='Our History'>
-            <Text style={{ margin: 10, fontSize: 16 }}>
+            <Text style={{ margin: 10 }}>
                 Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.{"\n\n"}
-            The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.
+            </Text>
+            <Text style={{ margin: 10 }}>
+                The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.
            </Text>
         </Card>
     );
-}
-
-function CorporateLeadership(props) {
-    const leaders = props.leaders;
-
-    if (leaders != null) {
-        const renderLeader = ({ item, index }) => {
-            return (
-                <ListItem
-                    key={index}
-                    title={item.name}
-                    titleStyle={{ fontWeight: 'bold' }}
-                    subtitle={item.description}
-                    subtitleStyle={{ color: '#6c757d' }} // #6c757d taken from Bootstrap blockquote-footer CSS
-                    hideChevron={true}
-                    leftAvatar={{ source: { uri: baseUrl + item.image } }}
-                />
-            );
-        };
-        return (
-            <Card title='Corporate Leadership'>
-                <FlatList
-                    data={this.props.leaders.leaders}
-                    renderItem={renderLeader}
-                    keyExtractor={item => item.id.toString()}
-                />
-            </Card>
-        );
-    }
-    else {
-        return (<View></View>)
-    }
 }
 
 class About extends Component {
@@ -60,16 +31,60 @@ class About extends Component {
     };
 
     render() {
+        const { params } = this.props.navigation.state;
+        const renderLeader = ({ item, index }) => {
 
+            return (
+                <ListItem
+                    roundAvatar
+                    key={index}
+                    title={item.name}
+                    subtitle={item.description}
+                    subtitleNumberOflines={15}
+                    hideChevron={true}
+                    leftAvatar={{ source: { uri: baseUrl + item.image } }}
+                />
+            );
+        };
 
-
-        return (
-            <ScrollView>
-                <History />
-                <CorporateLeadership leaders={this.state.leaders} />
-            </ScrollView>
-        );
-    };
-}
+        if (this.props.leaders.isLoading) {
+            return (
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else if (this.props.leaders.errMess) {
+            return (
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else {
+            return (
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <FlatList
+                            data={this.props.leaders.leaders}
+                            renderItem={renderLeader}
+                            keyExtractor={item => item.id.toString()}
+                        />
+                    </Card>
+                </ScrollView>
+            );
+        }
+    }
+};
 
 export default connect(mapStateToProps)(About);
