358b00f041109785ab01332e29d1db4357d4f56e
Nikhil Singh <singhyesh2000@gmail.com>
2020-11-19 15:13:14 +0530

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Alert } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
+import { Permissions, Notifications } from 'expo';
 
 const styles = StyleSheet.create({
     formRow: {
@@ -34,6 +35,33 @@ class Reservation extends Component {
         });
     }
 
+    static async obtainNotificationPermission() {
+        let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+        }
+        return permission;
+    }
+
+    static async presentLocalNotification(date) {
+        await Reservation.obtainNotificationPermission();
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: `Reservation for ${date} requested`,
+            ios: {
+                sound: true,
+            },
+            android: {
+                sound: true,
+                vibrate: true,
+                color: '#512DA8',
+            },
+        });
+    }
+
     constructor(props) {
         super(props);
         this.state = Reservation.defaultState();
@@ -43,8 +71,8 @@ class Reservation extends Component {
         this.setState(Reservation.defaultState());
     }
 
-    confirmReservation() {
-        // Stub for future code
+    confirmReservation(date) {
+        Reservation.presentLocalNotification(date);
         this.resetForm();
     }
 
@@ -63,7 +91,7 @@ class Reservation extends Component {
                 {
                     text: 'OK',
                     // eslint-disable-next-line no-confusing-arrow, no-console
-                    onPress: () => this.confirmReservation(),
+                    onPress: () => this.confirmReservation(date),
                 },
             ],
             { cancelable: false },
